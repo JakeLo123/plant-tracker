3c9e474fe86f29f9a163b4bd588414539c28f643
jakeloew <jakeloew1@gmail.com>
2019-12-13 22:52:25 -0600

    seeds database with try/catch

--- scripts/seed.js
+++ scripts/seed.js
@@ -2,14 +2,20 @@ const { db, Plant } = require('../server/db');
 const data = require('./Tandem_Data.json');
 
 async function seedDatabaseWithPlants() {
-  await db.sync({ force: true });
-  await Promise.all(
-    data.map(plant => {
-      const waterAfter = getDaysFromString(plant.water_after);
-      const p = { name: plant.name, waterAfter: waterAfter };
-      Plant.create(p);
-    })
-  );
+  try {
+    await db.sync({ force: true });
+    await Promise.all(
+      data.map(plant => {
+        const waterAfter = getDaysFromString(plant.water_after);
+        const p = { name: plant.name, waterAfter: waterAfter };
+        Plant.create(p);
+        console.log('created plant: ', p);
+      })
+    );
+    console.log('Seeding success! Seeds planted!');
+  } catch (e) {
+    console.error('error seeding database...', e);
+  }
 }
 
 function getDaysFromString(str) {
