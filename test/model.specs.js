16216d926bef7f64075c69344de65a6d9e77f3ad
jakeloew <jakeloew1@gmail.com>
2019-12-16 15:33:30 -0600

    unit tests for plant class and instance

--- test/model.specs.js
+++ test/model.specs.js
@@ -7,35 +7,43 @@ describe('Plant class', () => {
         await db.sync({ force: true })
         testPlant1 = {
             name: 'cucumber',
-            waterAfter: 3
+            waterAfter: 7
         }
     })
     it('creates an instance with a name, waterAfter, and receivedWaterOn properties', async () => {
         const cucumber = await Plant.create(testPlant1);
         expect(cucumber.name).to.equal('cucumber');
-        expect(cucumber.waterAfter).to.equal(3)
-        expect(cucumber.receivedWaterOnDates).to.deep.equal([])
+        expect(cucumber.waterAfter).to.equal(7);
+        expect(cucumber.receivedWaterOnDates).to.deep.equal([]);
     })
     describe('getSchedule instance method', () => {
         let cucumber, schedule;
         const oneDayMilliseconds = 86400000;
         beforeEach(async () => {
-            cucumber = await Plant.create(testPlant1)
-            schedule = cucumber.getSchedule()
+            cucumber = await Plant.create(testPlant1);
+            schedule = cucumber.getSchedule();
         })
-        it('returns an array of date strings', () => {
-            // schedule.forEach(element => {
-            //     assert.equal()
-            // })
+        it('does not include saturdays or sundays', () => {
+            function isWeekday(str){
+                const days = [ 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday' ];
+                return days.includes(str);
+            }
+            for(let i = 0; i < schedule.length; ++i){
+                let day = schedule[0].split(' ')[0];
+                assert.equal(isWeekday(day), true);
+            }
         })
-        it('returns an array of date strings on which the plant needs to be watered', () => {
-            
-        })
-        it('should not include saturdays or sundays', () => {
-
-        })
-        it('should last for 12 weeks', () => {
-            // const twelveWeeks = 7 * 12;
+        it('the interval between waterings is within 1 day of the plant\'s waterAfter property', () => {
+            const minInterval = (cucumber.waterAfter * oneDayMilliseconds) - oneDayMilliseconds;
+            const maxInterval = (cucumber.waterAfter * oneDayMilliseconds) + oneDayMilliseconds;
+            for(let i = 0; i < schedule.length-1; ++i) {
+                expect(
+                    Math.abs( Date.parse(schedule[i]) - Date.parse(schedule[i + 1]) )
+                )
+                .to
+                .be
+                .within(minInterval, maxInterval);
+            }
         })
     })
 })
