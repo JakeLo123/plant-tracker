d0db67251a481ae2521aedaad51b7fe0f3590230
jakeloew <jakeloew1@gmail.com>
2019-12-24 12:51:21 -0600

    refactoring for easy/clear testing

--- client/Main.js
+++ client/Main.js
@@ -1,105 +1,139 @@
 import React from 'react';
 import { Day, Week } from './components';
-import { makeScheduleFromPlants, getNextDay, stringifyDate, getWeekFromDay } from '../utils';
+import {
+  makeScheduleFromPlants,
+  getNextDay,
+  stringifyDate,
+  getWeekFromDay,
+} from '../utils';
 import axios from 'axios';
 
+export const selectToday = () => ({
+  visabilityFilter: 'day',
+  selectedDate: stringifyDate(new Date()),
+});
+
+export const selectNextDay = prevState => ({
+  visabilityFilter: 'day',
+  selectedDate: getNextDay(prevState.selectedDate),
+});
+
+export const selectDay = event => {
+  let selectedDate = event.target.value;
+  if (selectedDate === 'Today') {
+    selectedDate = stringifyDate(new Date());
+  }
+  return {
+    visabilityFilter: 'day',
+    selectedDate: selectedDate,
+  };
+};
+
 class Main extends React.Component {
-    constructor(){
-        super()
-        this.state = {
-            plants: [],
-            schedule: {},
-            selectedDate: stringifyDate(new Date()),
-            selectedWeek: getWeekFromDay(stringifyDate(new Date())),
-            visabilityFilter: 'day'
-        }
-        this.selectToday = this.selectToday.bind(this);
-        this.nextDay = this.nextDay.bind(this);
-        this.selectDay = this.selectDay.bind(this);
-        this.togglePlantWaterStatus = this. togglePlantWaterStatus.bind(this);
-        this.backToThisWeek = this.backToThisWeek.bind(this);
-    }
+  constructor() {
+    super();
+    this.state = {
+      plants: [],
+      schedule: {},
+      selectedDate: stringifyDate(new Date()),
+      selectedWeek: getWeekFromDay(stringifyDate(new Date())),
+      visabilityFilter: 'day',
+    };
+    this.selectToday = this.selectToday.bind(this);
+    this.nextDay = this.nextDay.bind(this);
+    this.selectDay = this.selectDay.bind(this);
+    this.togglePlantWaterStatus = this.togglePlantWaterStatus.bind(this);
+    this.backToThisWeek = this.backToThisWeek.bind(this);
+  }
 
-    componentDidMount(){
-        axios.get('/api/plants')
-            .then(res => {
-                let plants = res.data;
-                this.setState({
-                    plants: plants,
-                    schedule: makeScheduleFromPlants(plants)
-                })
-            })
-            .catch(e => {
-                console.error('error fetching plants...', e)
-            })
-    }
-
-    selectToday(){
+  componentDidMount() {
+    axios
+      .get('/api/plants')
+      .then(res => {
+        let plants = res.data;
         this.setState({
-            visabilityFilter: 'day',
-            selectedDate: stringifyDate(new Date())
-        })
-    }
+          plants: plants,
+          schedule: makeScheduleFromPlants(plants),
+        });
+      })
+      .catch(e => {
+        console.error('error fetching plants...', e);
+      });
+  }
 
-    nextDay(){
-        const selectedDate = this.state.selectedDate;
+  selectToday() {
+    this.setState(selectToday);
+  }
+
+  nextDay() {
+    this.setState(selectNextDay);
+  }
+
+  selectDay(event) {
+    this.setState(selectDay(event));
+  }
+
+  backToThisWeek() {
+    this.setState({
+      visabilityFilter: 'week',
+    });
+  }
+
+  togglePlantWaterStatus(plantId, dateToToggle) {
+    const plants = this.state.plants;
+    axios
+      .put(`/api/plants/${plantId}`, { dateToToggle })
+      .then(res => {
+        const updatedPlants = plants.map(plant => {
+          if (plant.name === res.data.name) return res.data;
+          else return plant;
+        });
         this.setState({
-            visabilityFilter: 'day',
-            selectedDate: getNextDay(selectedDate)
-        })
-    }
+          plants: updatedPlants,
+          schedule: makeScheduleFromPlants(updatedPlants),
+        });
+      })
+      .catch(e => console.log('error: ', e));
+  }
 
-    selectDay(event){
-        let selectedDate = event.target.value;
-        if (selectedDate === 'Today') {
-            selectedDate = stringifyDate(new Date())
-        }
-        this.setState({
-            visabilityFilter: 'day',
-            selectedDate: selectedDate
-        })
-    }
-
-    backToThisWeek(){
-        this.setState({
-            visabilityFilter: 'week'
-        })
-    }
-    
-    togglePlantWaterStatus(plantId, dateToToggle){
-        const plants = this.state.plants
-        axios.put(`/api/plants/${plantId}`, { dateToToggle })
-            .then(res => {
-                const updatedPlants = plants.map(plant => {
-                    if(plant.name === res.data.name) return res.data
-                    else return plant
-                })
-                this.setState({
-                    plants: updatedPlants,
-                    schedule: makeScheduleFromPlants(updatedPlants)
-                })
-            })
-            .catch(e => console.log('error: ', e))
-    }
-
-    render(){
-        const {schedule, selectedDate, selectedWeek, visabilityFilter} = this.state
-        return (
-            <div>
-                <header>
-                    <h1>we grow in tandem</h1>
-                    <div onClick={this.selectToday} className="pagination" >today</div>
-                    <div onClick={this.nextDay} className="pagination" >next day</div>
-                    <div onClick={this.backToThisWeek} className="pagination" >this week</div>
-                </header>
-                {
-                    visabilityFilter === 'day'
-                    ? (<Day selectDay={this.selectDay} togglePlantWaterStatus={this.togglePlantWaterStatus} schedule={schedule} selectedDate={selectedDate} />)
-                    : (<Week togglePlantWaterStatus={this.togglePlantWaterStatus} schedule={schedule} selectedWeek={selectedWeek} />)
-                }
-            </div>
-        )
-    }
+  render() {
+    const {
+      schedule,
+      selectedDate,
+      selectedWeek,
+      visabilityFilter,
+    } = this.state;
+    return (
+      <div>
+        <header>
+          <h1>we grow in tandem</h1>
+          <div onClick={this.selectToday} className="pagination">
+            today
+          </div>
+          <div onClick={this.nextDay} className="pagination">
+            next day
+          </div>
+          <div onClick={this.backToThisWeek} className="pagination">
+            this week
+          </div>
+        </header>
+        {visabilityFilter === 'day' ? (
+          <Day
+            selectDay={this.selectDay}
+            togglePlantWaterStatus={this.togglePlantWaterStatus}
+            schedule={schedule}
+            selectedDate={selectedDate}
+          />
+        ) : (
+          <Week
+            togglePlantWaterStatus={this.togglePlantWaterStatus}
+            schedule={schedule}
+            selectedWeek={selectedWeek}
+          />
+        )}
+      </div>
+    );
+  }
 }
 
 export default Main;
--- client/components/Day.js
+++ client/components/Day.js
@@ -1,27 +1,30 @@
-import React from 'react'
-import { stringifyDate, createDateOptions } from '../../utils'
+import React from 'react';
+import { stringifyDate, createDateOptions } from '../../utils';
 import PlantList from './PlantList';
 
-const Day = (props) => {
-    let {togglePlantWaterStatus, selectedDate, selectDay, schedule} = props;
-    const todaysDate = stringifyDate(new Date())
-    return (
-        <div id="day-component">
-            <select value={selectedDate} onChange={selectDay} className="pagination" >
-                <option>{ selectedDate === todaysDate ? 'Today' : selectedDate }</option>
-                        {
-                            createDateOptions().map(date => {
-                                return <option key={date}>{date}</option>
-                            })
-                        }
-            </select>
-            {
-                selectedDate.split(' ')[0] === 'Saturday' || selectedDate.split(' ')[0] === 'Sunday'
-                ? <h2>no watering on the weekends!</h2>
-                : <PlantList togglePlantWaterStatus={togglePlantWaterStatus} selectedDate={selectedDate} schedule={schedule} />
-            }
-        </div>
-    )
-}
+const Day = props => {
+  let { togglePlantWaterStatus, selectedDate, selectDay, schedule } = props;
+  const todaysDate = stringifyDate(new Date());
+  return (
+    <div id="day-component">
+      <select value={selectedDate} onChange={selectDay} className="pagination">
+        <option>{selectedDate === todaysDate ? 'Today' : selectedDate}</option>
+        {createDateOptions().map(date => {
+          return <option key={date}>{date}</option>;
+        })}
+      </select>
+      {selectedDate.split(' ')[0] === 'Saturday' ||
+      selectedDate.split(' ')[0] === 'Sunday' ? (
+        <h2>no watering on the weekends!</h2>
+      ) : (
+        <PlantList
+          togglePlantWaterStatus={togglePlantWaterStatus}
+          selectedDate={selectedDate}
+          schedule={schedule}
+        />
+      )}
+    </div>
+  );
+};
 
 export default Day;
