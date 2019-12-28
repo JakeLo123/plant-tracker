dfaa7e503a444b362ed81bf947799c8ce91a108e
jakeloew <jakeloew1@gmail.com>
2019-12-27 18:13:54 -0600

    configuring async tests

--- tests/component.specs.js
+++ tests/component.specs.js
@@ -5,45 +5,70 @@ import { stringifyDate, getWeekFromDay } from '../utils';
 import { Day, PlantList, Week } from '../client/components';
 import { shallow, mount, render, configure } from 'enzyme';
 import Adapter from 'enzyme-adapter-react-16';
-// import axios from 'axios';
-// import { stub } from 'sinon';
-
+import axios from 'axios';
+import { fakeServer, stub } from 'sinon';
 configure({ adapter: new Adapter() });
 
-describe('Main component', () => {
-  let wrapper;
-  beforeEach(() => {
-    wrapper = shallow(<Main />);
+const mockData = {
+  data: [
+    {
+      name: 'banana',
+      waterAfter: 2,
+    },
+    {
+      name: 'cucumber',
+      waterAfter: 7,
+    },
+  ],
+};
+
+describe('pagination', () => {
+  let fetch, wrapper, promise;
+  before(() => {
+    promise = Promise.resolve(mockData);
+    fetch = stub(axios, 'get')
+      .withArgs('/api/plants')
+      .returns(promise);
+    wrapper = mount(<Main />);
   });
-  describe('local state', () => {
-    it('should initialize with `selectedDate` set to the current date as a string', () => {
-      const currentDate = stringifyDate(new Date());
-      expect(wrapper.state().selectedDate).to.equal(currentDate);
-    });
-    it('should initialize with `selectedWeek` set to the current an array of dates representing the current week', () => {
-      const currentDate = stringifyDate(new Date());
-      const currentWeek = getWeekFromDay(currentDate);
-      expect(wrapper.state().selectedWeek).to.deep.equal(currentWeek);
-    });
-    describe('methods for setting state', () => {
-      it('`selectDate` should set visibilityFilter to `day` and select any date given as a string', () => {
-        const testState = selectDate('Wednesday December 25, 2019');
-        expect(testState.selectedDate).to.equal('Wednesday December 25, 2019');
-        expect(testState.visibilityFilter).to.equal('day');
-      });
-      it('`selectWeek` should set visibilityFilter to `week`', () => {
-        const testState = selectWeek();
-        expect(testState.visibilityFilter).to.equal('week');
-      });
-    });
-    describe('pagination', () => {
-      it('displays this week after click', () => {
-        const thisWeekButton = wrapper.find('#this-week-btn');
-        expect(wrapper.find(Week)).to.have.length(0);
-        thisWeekButton.simulate('click');
-        expect(wrapper.find(Week)).to.have.length(1);
-      });
-      it('displays the correct day', () => {});
-    });
+  after(() => {
+    // server.restore();
+    axios.get.restore();
   });
+  it('displays this week after click', () => {
+    // const thisWeekButton = wrapper.find('#this-week-btn');
+    // expect(wrapper.find(Week)).to.have.length(0);
+    // thisWeekButton.simulate('click');
+    // expect(wrapper.find(Week)).to.have.length(1);
+  });
+  it('displays the correct day', () => {});
 });
+
+// describe('Main component', () => {
+//   let wrapper;
+//   beforeEach(() => {
+//     wrapper = shallow(<Main />, { disableLifecycleMethods: true });
+//   });
+//   describe('local state', () => {
+//     it('should initialize with `selectedDate` set to the current date as a string', () => {
+//       const currentDate = stringifyDate(new Date());
+//       expect(wrapper.state().selectedDate).to.equal(currentDate);
+//     });
+//     it('should initialize with `selectedWeek` set to the current an array of dates representing the current week', () => {
+//       const currentDate = stringifyDate(new Date());
+//       const currentWeek = getWeekFromDay(currentDate);
+//       expect(wrapper.state().selectedWeek).to.deep.equal(currentWeek);
+//     });
+//     describe('methods for setting state', () => {
+//       it('`selectDate` should set visibilityFilter to `day` and select any date given as a string', () => {
+//         const testState = selectDate('Wednesday December 25, 2019');
+//         expect(testState.selectedDate).to.equal('Wednesday December 25, 2019');
+//         expect(testState.visibilityFilter).to.equal('day');
+//       });
+//       it('`selectWeek` should set visibilityFilter to `week`', () => {
+//         const testState = selectWeek();
+//         expect(testState.visibilityFilter).to.equal('week');
+//       });
+//     });
+//   });
+// });
